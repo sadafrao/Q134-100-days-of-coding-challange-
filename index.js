39214194d12a484fe69ac870de391a77b3243503
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 18:11:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Q134
+// define j sons string
+var jsonstring = '{"name": "John", "age": 30, "city": "New York"}';
+var json = JSON.parse(jsonstring);
+console.log(json.name);
---
+++ index.ts
@@ -0,0 +1,5 @@
+// Q134
+// define j sons string
+const jsonstring = '{"name": "John", "age": 30, "city": "New York"}';
+const json = JSON.parse(jsonstring);
+console.log(json.name);
